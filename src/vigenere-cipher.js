8f24578919e0ef5e88f5e535dd44a7c96d31e58c
a.karavaychik@gmail.com <a.karavaychik@gmail.com>
2021-03-30 00:17:13 +0300

    fix: fix cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -4,8 +4,8 @@ class VigenereCipheringMachine {
   constructor(isDirect = true) {
     this.isDirect = isDirect;
     this.square = {};
-    this.alpabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    this.alpabetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.alpabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    this.alpabetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
   encrypt(str, word) {
@@ -13,31 +13,34 @@ class VigenereCipheringMachine {
     let arrStr = str.split("");
     let wordStr = word.split("");
     let newText = "";
+    let strSpaceCount = 0;
+    let wordSpaceCount = 0;
 
     if (word.length<str.length) {
       wordStr = word.repeat(Math.ceil(str.length/word.length)).split("")
       wordStr.length=str.length;
-    } else arrStr.length=wordStr.length;
+    } else wordStr.length=arrStr.length;
 
     console.log(wordStr)
     console.log(arrStr)
 
 
     for (let i = 0; i<arrStr.length; i++) {
-      if (this.alpabetStr.indexOf(arrStr[i].toUpperCase())===-1 || this.alpabetStr.indexOf(wordStr[i].toUpperCase())===-1) {
+      if (this.alpabetStr.indexOf(arrStr[i].toUpperCase())===-1) {
         newText+=arrStr[i];
+        strSpaceCount++;
+        continue
       }
-      else {
-        newText += this.alpabet[this.alpabetStr.indexOf(arrStr[i].toUpperCase())+this.alpabetStr.indexOf(wordStr[i].toUpperCase())]
+      if (this.alpabetStr.indexOf(wordStr[i].toUpperCase())===-1) {
+        newText+=arrStr[i];
+        continue
       }
+
+      newText += this.alpabet[this.alpabetStr.indexOf(arrStr[i].toUpperCase())+this.alpabetStr.indexOf(wordStr[i-strSpaceCount].toUpperCase())]
+
     }
     return newText;
-
-
-
-    // throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
+  }
   decrypt() {
     throw new CustomError('Not implemented');
     // remove line with error and write your code here
