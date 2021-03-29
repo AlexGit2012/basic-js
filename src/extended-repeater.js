daad4984a8609bf1ed5ad7cddfec38b5da925ad1
a.karavaychik@gmail.com <a.karavaychik@gmail.com>
2021-03-29 23:54:20 +0300

    feat: work with vigenere-cipher

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,24 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  str1 = `${str}`
-  let separator;
-  let additionSeparator;
-  let repeatTimes = options.repeatTimes || 1;
-  let additionRepeatTimes = options.additionRepeatTimes || 1;
-  let addition = options.addition || ``;
-  if (options.addition===false) addition = `${options.addition}`
-  if (options.separator===undefined) separator="+"; else separator=options.separator
-  if (options.additionSeparator===undefined) additionSeparator="|"; else additionSeparator=options.additionSeparator
-  // throw new CustomError('Not implemented');
-  // // remove line with error and write your code here
-  let res = "";
-  let additionArr = []
-  let resArr = []
-  for (let i =0; i<additionRepeatTimes; i++) additionArr.push(addition.toString())
-  res = `${str1.toString()}${additionArr.join(additionSeparator)}`
-  for (let i=0; i<repeatTimes; i++) resArr.push(res)
-  res = resArr.join(separator)
-  return res;
+
+    let tempStr = (str === null) ? "null" : str.toString();
+    let tempAddition = (options.addition === null) ? "null" : (options.addition===false ? "false" : (options.addition ? options.addition.toString() : ""));
+    let tempSeparator = options.separator ? options.separator.toString() : "+";
+    let tempAdditionSeparator = options.additionSeparator ? options.additionSeparator.toString() : "|";
+    let tempRepeatTimes = options.repeatTimes ? (options.repeatTimes.isNaN ? 0 : options.repeatTimes) : 1;
+    let tempAdditionRepeatTimes = options.additionRepeatTimes ? (options.additionRepeatTimes.isNaN ? 0 : options.additionRepeatTimes) : 1;
+
+    let newAddition = [];
+    for (let i = 0; i < tempAdditionRepeatTimes; i++) {
+        newAddition.push(tempAddition)
+    }
+    newAddition = newAddition.join(tempAdditionSeparator);
+
+
+    let newStr = [];
+    for (let i = 0; i < tempRepeatTimes; i++) {
+        newStr.push(tempStr + newAddition)
+    }
+    newStr = newStr.join(tempSeparator)
+
+    return newStr;
+
+
+    // str1 = `${str}`
+    // let separator;
+    // let additionSeparator;
+    // let repeatTimes = options.repeatTimes || 1;
+    // let additionRepeatTimes = options.additionRepeatTimes || 1;
+    // let addition = options.addition || ``;
+    // if (options.addition===false) addition = `${options.addition}`
+    // if (options.separator===undefined) separator="+"; else separator=options.separator
+    // if (options.additionSeparator===undefined) additionSeparator="|"; else additionSeparator=options.additionSeparator
+    // // throw new CustomError('Not implemented');
+    // // // remove line with error and write your code here
+    // let res = "";
+    // let additionArr = []
+    // let resArr = []
+    // for (let i =0; i<additionRepeatTimes; i++) additionArr.push(addition.toString())
+    // res = `${str1.toString()}${additionArr.join(additionSeparator)}`
+    // for (let i=0; i<repeatTimes; i++) resArr.push(res)
+    // res = resArr.join(separator)
+    // return res;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,8 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.square = {};
+    this.alpabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    this.alpabetStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  encrypt(str, word) {
+
+    let arrStr = str.split("");
+    let wordStr = word.split("");
+    let newText = "";
+
+    if (word.length<str.length) {
+      wordStr = word.repeat(Math.ceil(str.length/word.length)).split("")
+      wordStr.length=str.length;
+    } else arrStr.length=wordStr.length;
+
+    console.log(wordStr)
+    console.log(arrStr)
+
+
+    for (let i = 0; i<arrStr.length; i++) {
+      if (this.alpabetStr.indexOf(arrStr[i].toUpperCase())===-1 || this.alpabetStr.indexOf(wordStr[i].toUpperCase())===-1) {
+        newText+=arrStr[i];
+      }
+      else {
+        newText += this.alpabet[this.alpabetStr.indexOf(arrStr[i].toUpperCase())+this.alpabetStr.indexOf(wordStr[i].toUpperCase())]
+      }
+    }
+    return newText;
+
+
+
+    // throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }    
   decrypt() {
